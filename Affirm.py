34dd58dd10ad535af07e4e157f7b4b7cf4513dc3
michelleduong03 <miichelleduong@gmail.com>
2025-12-05 11:34:10 -0800

    practice problems

---
+++ Affirm.py
@@ -0,0 +1,36 @@
+# two_sum.py
+
+from typing import List
+
+def two_sum(nums: List[int], target: int) -> List[int]:
+    # TODO: implement your solution
+    pass
+
+# Example test
+nums = [2, 7, 11, 15]
+target = 9
+print(two_sum(nums, target))  # Output: [0, 1]
+
+
+# longest_substring.py
+
+def length_of_longest_substring(s: str) -> int:
+    # TODO: implement your solution
+    pass
+
+# Example test
+s = "abcabcbb"
+print(length_of_longest_substring(s))  # Output: 3
+
+
+# merge_intervals.py
+
+from typing import List
+
+def merge_intervals(intervals: List[List[int]]) -> List[List[int]]:
+    # TODO: implement your solution
+    pass
+
+# Example test
+intervals = [[1,3],[2,6],[8,10],[15,18]]
+print(merge_intervals(intervals))  # Output: [[1,6],[8,10],[15,18]]
