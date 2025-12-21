3aa3da9cf66830bf3b9ab46f33c2ab09f8e7dff3
michelleduong03 <miichelleduong@gmail.com>
2025-12-20 16:13:51 -0800

    max depth dfs

--- DFS.py
+++ DFS.py
@@ -237,7 +237,7 @@ def letterCombinations(digits):
         letters = phone[digits[i]]
 
         # Try each letter
-        for ch in letters:
+        for ch in letters: 
             dfs(i + 1, path + ch)  # move to next digit
 
     dfs(0, "")
@@ -246,3 +246,19 @@ def letterCombinations(digits):
 
 # Test
 print(letterCombinations("23"))
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def maxDepth(root):
+    if root is None:
+        return 0
+
+    left_depth = maxDepth(root.left)
+    right_depth = maxDepth(root.right)
+
+    return 1 + max(left_depth, right_depth)
+
